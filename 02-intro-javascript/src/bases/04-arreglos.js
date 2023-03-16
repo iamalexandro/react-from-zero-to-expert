4c76869edac5a742a31121c46a6c8d0cae121d5c
Nicola Di Candia <nicoladicandiajaimes@gmail.com>
2023-03-16 16:45:10 -0500

    adding arrays section

---
+++ 02-intro-javascript/src/bases/04-arreglos.js
@@ -0,0 +1,20 @@
+//Arreglos en Js
+
+// const arreglo = new Array(100);
+
+const arreglo = [1, 2, 3, 4];
+// arreglo.push(1);
+// arreglo.push(2);
+
+let arreglo2 = [...arreglo, 5];
+// arreglo2.push(5);
+
+// const arreglo3 = arreglo2.map((numeros) => numeros + 1);
+// const arreglo3 = arreglo2.map((numero) => numero * 2);
+const arreglo3 = arreglo2.map(function (numero) {
+  return numero * 2;
+});
+
+console.log(arreglo);
+console.log(arreglo2);
+console.log(arreglo3);
