c891bbb4899159a2600bb2c9418f332c887dc0aa
Nicola Di Candia <nicoladicandiajaimes@gmail.com>
2023-03-21 17:17:48 -0500

    ternary section added

---
+++ 02-intro-javascript/src/bases/12-ternario.js
@@ -0,0 +1,13 @@
+const active = true;
+
+// let message;
+
+// if (active) {
+//   message = "Active";
+// } else {
+//   message = "Inactive";
+// }
+
+// const message = active ? "Active" : "Inactive";
+
+const message = active && console.log("Active");
